3caa32efd5d2ff354fd07c9e9a5ca060eb1de327
Darrin <darrin.lovard.allen@gmail.com>
2024-05-14 22:54:11 -0700

    jwt

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -50,6 +50,7 @@ router.post('/login', async (req, res, next) => {
     const [user] = await Users.findBy({ username }).first();
 
     if (!user) {
+
       // If username doesn't exist
       return res.status(401).json({ message: 'Invalid credentials' });
     }
@@ -62,10 +63,10 @@ router.post('/login', async (req, res, next) => {
     }
 
     // If username and password are correct, generate JWT token
-   const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+   //const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
     // Send the JWT token in the response
-    res.status(200).json({ message: 'Login successful', token});
+    res.status(200).json({ message: 'Login successful'});
     
   } catch (error) {
     // Pass the error to the error handling middleware
